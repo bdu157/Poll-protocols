52f84d17c0eddd67db245809125bc787570c3604
Dongwoo Pae <dongwoopae@gmail.com>
2019-04-28 14:45:11 -0700

    created protocol

---
+++ Poll/VoteControllerProtocols.swift
@@ -0,0 +1,14 @@
+//
+//  VoteControllerProtocols.swift
+//  Poll
+//
+//  Created by Dongwoo Pae on 4/28/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import Foundation
+
+protocol VoteControllerProtocols {
+    var voteController: VoteController? {get set}
+}
+
