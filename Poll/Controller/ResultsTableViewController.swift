206117e7d3f8348b358ac23f9a0c10a2723630fa
Dongwoo Pae <dongwoopae@gmail.com>
2019-04-28 15:16:19 -0700

    setting up ResultsTableVie - numberOfRowsInSection and cellForRowAt

--- Poll/Controller/ResultsTableViewController.swift
+++ Poll/Controller/ResultsTableViewController.swift
@@ -15,24 +15,25 @@ class ResultsTableViewController: UITableViewController, VoteControllerProtocols
         super.viewDidLoad()
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return voteController?.votes.count ?? 0 // if there is no input then return 0 ternary operator
     }
 
  
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RightDetail", for: indexPath)
+        guard let vote = voteController?.votes[indexPath.row] else {return UITableViewCell()}
+        cell.textLabel?.text = vote.name
+        cell.detailTextLabel?.text = vote.response
+        
         return cell
     }
 
